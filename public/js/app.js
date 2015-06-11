7d9c0355df6834471aafb4967c2ed01453113728
Liam Kaufman <liam.kaufman@gmail.com>
2015-06-11 02:14:29 +0000

    can save entries

--- public/js/app.js
+++ public/js/app.js
@@ -1,12 +1,82 @@
-var app = angular.module('meanBlog', []);
+var app = angular.module('meanBlog', ['ngRoute']);
+
+app.config(function($routeProvider){
+  $routeProvider
+    .when('/', {
+      templateUrl : 'templates/home.html',
+      controller : 'EntriesIndexCtrl'
+    })
+    .when('/entry/create', {
+      templateUrl : 'templates/entry-form.html',
+      controller : 'EntriesCreateCtrl'
+    })
+    .when('/entry/:id', {
+      templateUrl : 'templates/entry.html',
+      controller : 'EntriesShowCtrl'
+    })
+    .otherwise('/');
+});
 
 app.controller('EntriesIndexCtrl', 
-function($scope){
+function($scope, BlogEntries){
 
-  $scope.entries = [
-    {title : 'First entry', body : 'This is my first blog post'},
-    {title : 'Second', body: 'A classic example of another....'},
-    {title : 'Oldy', body: 'How could we not forget writing...'}
-  ];
-  
+  BlogEntries.all().then(function(entries){
+    $scope.entries = entries;
+  });
+
+});
+
+app.controller('EntriesShowCtrl', 
+function($scope, $routeParams, BlogEntries){
+  $scope.routeParams = $routeParams;
+
+  BlogEntries.find($routeParams.id).then(function(entry){
+    $scope.entry = entry;
+  });
+});
+
+app.controller('EntriesCreateCtrl',
+function($scope, BlogEntries){
+  $scope.entry = {
+    title : '',
+    body: ''
+  }
+
+  $scope.save = function(entry){
+    BlogEntries.create(entry).then(function(){
+      console.log('success');
+    });
+  };
+});
+
+app.factory('BlogEntries', function($http){
+  var service = {};
+
+  service.all = function(){
+    return $http({
+      url : '/api/blog-entries/',
+      method : 'GET'
+    }).then(function(response){
+      return response.data;
+    });
+  };
+
+  service.find = function(id){
+    return $http({
+      url : '/api/blog-entries/' + id,
+      method : 'GET'
+    }).then(function(response){
+      return response.data;
+    });
+  };
+
+  service.create = function(entry){
+    return $http({
+      url : '/api/blog-entries/',
+      method : 'POST',
+      data : entry
+    });
+  }
+
+  return service;
 });
